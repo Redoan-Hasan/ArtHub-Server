df86be5133e2e18872ec638940f8ed43c60066c3
Redoan Hasan <redoanhasan57@gmail.com>
2024-11-12 07:52:59 +0600

    completed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors({
-    origin: ["http://localhost:5173" , "https://art-hub-server.vercel.app"]
+    origin: ["http://localhost:5173" , "https://art-hub-server.vercel.app" , "https://arthub-d9977.web.app"]
 }))
 app.use(express.json())
 
